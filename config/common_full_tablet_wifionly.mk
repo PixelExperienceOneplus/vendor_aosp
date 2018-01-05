DirtyUnicorns/android_vendor_du

Add tablet wifi only config

config/common_full_tablet_wifionly.mk
@@ -1,0 +1,2 @@
+# Inherit common stuff
+$(call inherit-product, vendor/du/config/common.mk)

PixelExperienceOneplus/vendor_aosp

config/common_full_tablet_wifionly.mk
@@ -1,0 +1,2 @@
+# Inherit common stuff
+$(call inherit-product, vendor/aosp/config/common.mk)